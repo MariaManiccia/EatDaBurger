8e80e94eda4f39895958493fb39deca04d6e6aa5
Maniccia <mariamaniccia@gmail.com>
2019-02-14 15:00:43 -0500

    mySQL files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+
+USE burgers_db;
+
+
+INSERT INTO burgers (burger_name, devoured, date) VALUES 
+('Jalapeno', false, CURRENT_TIMESTAMP),
+('Black Pig', false, CURRENT_TIMESTAMP),
+('Black Bean', false, CURRENT_TIMESTAMP);
