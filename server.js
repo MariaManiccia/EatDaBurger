3e0c699f5c2e5da9077791e2728f0c207120dcba
Maniccia <mariamaniccia@gmail.com>
2019-02-14 14:38:48 -0500

    inital server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+// Main JS file
+
+// grabbing express
+const express = require("express");
+
+// opening the port
+const PORT = process.env.PORT || 3030;
+
+// calling express
+const app = express();
+
+// giving the app direction
+app.use(express.static(__dirname + "/public"));
+
+// break apart into JSON
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// express-handlebars
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// router
+let router = require('./controllers/burgers_controller.js');
+app.use('/', router);
+
+
+// communicate connection
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
