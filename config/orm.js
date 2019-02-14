cce98af8288a3d0a30d497fd7521c334627edd52
Maniccia <mariamaniccia@gmail.com>
2019-02-14 14:47:38 -0500

    orm.js complete

---
+++ config/orm.js
@@ -0,0 +1,38 @@
+// grab connection
+const connection = require('../config/connection.js')
+
+// communication with the database
+const orm = {
+	
+	selectAll: function (callback) {
+		connection.query('SELECT * FROM burgers', function (err, result) {
+			if (err) throw err;
+			callback(result);
+		});
+	},
+
+	
+	insertOne: function (burger_name, callback) {
+		connection.query('INSERT INTO burgers SET ?',
+			{
+				burger_name: burger_name,
+				devoured: false,
+			}, function (err, result) {
+				if (err) throw err;
+				callback(result);
+			});
+
+	},
+
+	
+	updateOne: function (burgerID, callback) {
+		connection.query('UPDATE burgers SET ? WHERE ?', [{ devoured: true }, { id: burgerID }],
+			function (err, result) {
+				if (err) throw err;
+				callback(result);
+			});
+	}
+};
+
+
+module.exports = orm;
